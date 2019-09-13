e23269599c0e885145dc5a2a6eef550afed30a70
TolgaSener <32538300+TolgaSener@users.noreply.github.com>
2019-09-13 17:41:22 +0300

    Add files via upload

---
+++ BaseLibrary.Data/BaseDbOfWorks.cs
@@ -0,0 +1,35 @@
+﻿using BaseLibrary.Data.Contracts;
+using BaseLibrary.Data.Contracts.DataBase;
+using BaseLibrary.Data.Contracts.StoredProcedure;
+using BaseLibrary.Data.Models.Config;
+using SAPbobsCOM;
+namespace BaseLibrary.Data
+{
+   public class BaseDbOfWorks: IBaseDbOfWorks
+    {
+        private static Company Company;
+        public Company company
+        {
+            get { return Company = Company ?? new SAPbobsCOM.Company(); }
+            set { Company = value; }
+        }
+        public IRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : class
+        {
+            return new RepositoryBase<TEntity, TKey>();
+        }
+        public Company GetSapRepository(CompanyResquest request)
+        {
+            if (company.Connected == false)
+            {
+                SapRepository sapb1repo = new SapRepository(company);
+                Company = sapb1repo.SAPCompanyConnection(request);
+            }
+            return Company;
+        }
+       
+        public IBaseProcedure GetStoredProcedure()
+        {
+            return  new BaseProcedure(new CheckOutEntities());
+        }
+    }
+}
---
+++ BaseLibrary.Data/IBaseDbOfWorks.cs
@@ -0,0 +1,14 @@
+﻿using BaseLibrary.Data.Contracts;
+using BaseLibrary.Data.Contracts.StoredProcedure;
+using BaseLibrary.Data.Models.Config;
+using SAPbobsCOM;
+namespace BaseLibrary.Data
+{
+    public interface IBaseDbOfWorks
+    {
+        IRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : class;
+        IBaseProcedure GetStoredProcedure();
+        Company GetSapRepository(CompanyResquest request);
+
+    }
+}
