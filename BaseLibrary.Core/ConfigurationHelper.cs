fd42054bf29af89817303e588e6a303ddc0cd274
TolgaSener <32538300+TolgaSener@users.noreply.github.com>
2019-09-13 17:39:22 +0300

    Add files via upload

---
+++ BaseLibrary.Core/ConfigurationHelper.cs
@@ -0,0 +1,15 @@
+﻿using BaseLibrary.Core.Contracts;
+using SAPbobsCOM;
+using System.Configuration;
+using System.Globalization;
+namespace BaseLibrary.Core { 
+   public class ConfigurationHelper: IConfigurationHelper
+    {
+        public string SapServer => ConfigurationManager.AppSettings["sapServer"].ToString(CultureInfo.InvariantCulture);
+        public string SapLinsansServer => ConfigurationManager.AppSettings["sapLinsansServer"].ToString(CultureInfo.InvariantCulture);
+        public string CompanyDB => ConfigurationManager.AppSettings["companyDB"].ToString(CultureInfo.InvariantCulture);      
+        public string DbUserName => ConfigurationManager.AppSettings["dbUserName"].ToString(CultureInfo.InvariantCulture);
+        public string DbPassword => ConfigurationManager.AppSettings["dbUserName"].ToString(CultureInfo.InvariantCulture);
+        BoDataServerTypes IConfigurationHelper.DbServerType => BoDataServerTypes.dst_MSSQL2014;
+    }
+}
---
+++ BaseLibrary.Core/DependencyContainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+
+namespace BaseLibrary.Core
+{
+    public static class DependencyContainer
+    {
+        public static IWindsorContainer WindsorContainer { get; private set; }
+        public static FromAssemblyDescriptor Descriptor { get; private set; }
+
+        private static bool _bootstrapped;
+
+        public static void Bootstrap()
+        {
+            if (_bootstrapped) return;
+            WindsorContainer = new WindsorContainer();
+
+            var assemblyFilter = new AssemblyFilter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin"));
+
+            Descriptor = Classes.FromAssemblyInDirectory(assemblyFilter);
+
+            WindsorContainer.Install(FromAssembly.InDirectory(assemblyFilter));
+
+            _bootstrapped = true;
+        }
+
+        public static T Resolve<T>()
+        {
+            return WindsorContainer.Resolve<T>();
+        }
+
+        public static object Resolve(Type type)
+        {
+            return WindsorContainer.Resolve(type);
+        }
+    }
+}
